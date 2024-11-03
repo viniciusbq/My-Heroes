b5ee392ab8711ba73e3a3f969f996f8725eb78d8
VINICIUS QUEIROZ <borbaqueirozvinicius@gmail.com>
2024-11-03 20:49:22 -0300

    feat: add error page

--- src/pages/Home/heroesList/heroeCard/styles.ts
+++ src/pages/Home/heroesList/heroeCard/styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes, css } from 'styled-components';
+import styled from 'styled-components';
 import { TagContainerProps } from '../../../../types';
 
 export const HeroesCard = styled.div<TagContainerProps>`
